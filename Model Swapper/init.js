b404dfab0b569cd756e43c5818ac57bf1cead437
Tehchy <juniortec21@yahoo.com>
2019-03-05 02:15:19 -0500

    Auto change ao and e

--- Model Swapper/init.js
+++ Model Swapper/init.js
@@ -1,12 +1,12 @@
 let replace = [
-    // OLD, NEW, AO, E
-    ['weapon_2', 'model', false, false], 
-    ['weapon_3', 'model', false, false], 
+    // OLD, NEW
+    ['weapon_2', 'model'], 
+    ['weapon_3', 'model'], 
 ];
 let request = chrome.webRequest.onBeforeRequest;
 for (let rep of replace) {
     request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + ".obj")}}, {urls: ["https://krunker.io/models/weapons/" + rep[0] + ".obj"]}, ["blocking"]);
     request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + ".png")}}, {urls: ["https://krunker.io/textures/weapons/" + rep[0] + ".png"]}, ["blocking"]);
-    if (rep[2]) request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + "_ao.png")}}, {urls: ["https://krunker.io/textures/weapons/" + rep[0] + "_ao.png"]}, ["blocking"]);
-    if (rep[3]) request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + "_e.png")}}, {urls: ["https://krunker.io/textures/weapons/" + rep[0] + "_e.png"]}, ["blocking"]);
+    request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + "_ao.png")}}, {urls: ["https://krunker.io/textures/weapons/" + rep[0] + "_ao.png"]}, ["blocking"]);
+    request.addListener(function(details) {return {redirectUrl:chrome.extension.getURL(rep[1] + "_e.png")}}, {urls: ["https://krunker.io/textures/weapons/" + rep[0] + "_e.png"]}, ["blocking"]);
 }
